082ed1400868fa48c00b06dce5b2b99e13d783fa
chris-mathevet <chris.mathevet@etu.univ-orleans.fr>
2025-03-21 16:51:13 +0100

    popUp

--- client/assets/script/view/Detail.js
+++ client/assets/script/view/Detail.js
@@ -5,6 +5,7 @@ import BaseView from './BaseView.js';
 import UserManagment from '../utils/UserManagement.js';
 import Utils from '../utils/Utils.js';
 import { ENDPOINT } from '../config.js';
+import { showPopUp } from '../utils/popUp.js';
 
 export default class Detail extends BaseView{
     static updating = false;
@@ -325,7 +326,7 @@ export default class Detail extends BaseView{
                     shoes: 0 
                 };
             }
-            }
+        }
 
             
         } catch (error) {}
@@ -358,9 +359,10 @@ export default class Detail extends BaseView{
         });
 
         document.getElementById("createCharacterButton").addEventListener("click", (event)=>{
+            event.target.disabled = true;
             event.preventDefault();
             console.log(Detail.centerIndex)
-            CharacterManagement.createOrUpdateCharacter(
+            let succes = CharacterManagement.createOrUpdateCharacter(
                 document.getElementById("inputName").value,
                 Detail.equipments.head[Detail.centerIndex.head].id,
                 Detail.equipments.torso[Detail.centerIndex.torso].id,
@@ -369,6 +371,20 @@ export default class Detail extends BaseView{
                 UserManagment.getUsername(),
                 character ? character.id : undefined
             )
+            let message = "";
+            if(succes){
+                if(character){
+                    message = "Personnage modifié avec succès";
+                }
+                else{
+                    message = "Personnage crée avec succès";
+                }
+            }
+            else{
+                message = "Création impossible"
+            }
+            showPopUp(message, succes);
+            window.location.href = "#/listing";
         })
     }
 
